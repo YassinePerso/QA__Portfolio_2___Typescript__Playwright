b327d6d5b5459da3e2379bf083bda518886d5bae
Yass <yassineboulakhrif75@gmail.com>
2026-08-06 21:20:54 +0200

    FEAT: Register tests DONE

--- Sprint_1___Access-management/automatisation/fixtures/test-data.ts
+++ Sprint_1___Access-management/automatisation/fixtures/test-data.ts
@@ -1,16 +1,15 @@
- export const TEST_USER = {
+export const TEST_USER = {
   firstname: 'Yassine',
   lastname: 'Test',
   birthdate: '1990-01-01',
   country: 'France',
-  postalcode: '75001',
-  streetnumber: '42',
-  streetname: 'Rue de la Paix',
+  postcode: '75001',
+  address: '42 Rue de la Paix',
   city: 'Paris',
   state: 'Île-de-France',
-  phonenumber: '0600000000',
-  email: 'qa.portfolio.test@test.com',
-  password: 'QaP0rtf0li0!2026',
+  phone: '0600000000',
+  email: 'demowebshop.qa.test@gmail.com',
+  password: '7S!8VehsK5rtqT',
 };
 
 export const URLS = {
